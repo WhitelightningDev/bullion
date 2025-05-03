9579140dc9d200ced252699922b25c739a6f0a53
WhitelightningDev <danielmommsen@hotmail.com>
2025-05-03 10:01:40 +0200

    added the login button for users who have already logged into myco-op

--- src/app/downloads/downloads.component.ts
+++ src/app/downloads/downloads.component.ts
@@ -15,4 +15,9 @@ export class DownloadsComponent {
     const url = 'https://rbkam-my.sharepoint.com/personal/willem_rbkam_onmicrosoft_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fwillem%5Frbkam%5Fonmicrosoft%5Fcom%2FDocuments%2FCompanies%2FMy%20Co%2Dop%2FMy%20Coop%20ASPNET%2FInformation%20for%20users%2FRegistration%20Guide%20on%20My%20Coop%2Epdf&parent=%2Fpersonal%2Fwillem%5Frbkam%5Fonmicrosoft%5Fcom%2FDocuments%2FCompanies%2FMy%20Co%2Dop%2FMy%20Coop%20ASPNET%2FInformation%20for%20users&ga=1';
     window.open(url, '_blank');  // This opens the link in a new tab
   }
+
+  newMemberLogin(){
+    const url = 'https://system.mycoop.co.za/account/login'
+    window.open(url, '_blank');
+  }
 }
