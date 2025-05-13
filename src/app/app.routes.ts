fcca70de87bdf3e1d027df8c370ea983e75bcf23
whitelightningdev <danielmommsen@hotmail.com>
2025-05-13 21:34:51 +0200

    added the oute for the faq page and created the faq page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { PoolfeaturesComponent } from './poolfeatures/poolfeatures.component';
 import { FeesComponent } from './fees/fees.component';
 import { DownloadsComponent } from './downloads/downloads.component';
 import { ContactusComponent } from './contactus/contactus.component';
+import { FaqComponent } from './faq/faq.component';
 
 export const routes: Routes = [
   { path: "", component: HomepageComponent, data: { animation: 'Home' } },
   { path: "poolfeatures", component: PoolfeaturesComponent, data: { animation: 'PoolFeatures' } },
   { path: "fees", component: FeesComponent, data: { animation: 'Fees' } },
   { path: "downloads", component: DownloadsComponent, data: { animation: 'Downloads' } },
-  { path: "contact-us", component: ContactusComponent, data: { animation: 'Contact' } }
+  { path: "contact-us", component: ContactusComponent, data: { animation: 'Contact' } },
+  { path: "faq", component: FaqComponent, data: { animation: 'FAQ ' } },
 ];
---
+++ src/app/faq/faq.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-faq',
+  imports: [CommonModule],
+  templateUrl: './faq.component.html',
+  styleUrl: './faq.component.css'
+})
+export class FaqComponent {
+
+}
