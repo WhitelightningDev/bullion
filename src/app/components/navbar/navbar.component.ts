b2715d9845b49b1f82abef92610a66fa0ba4999e
whitelightningdev <danielmommsen@hotmail.com>
2025-05-15 20:23:04 +0200

    `Refactor navbar component to close on navigation link click and improve responsiveness`

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild, Renderer2, AfterViewInit } from '@angular/core';
 import { RouterModule } from "@angular/router";
 import { RegisterpopupComponent } from "../../popups/registerpopup/registerpopup.component";
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [RouterModule, RegisterpopupComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements AfterViewInit {
+  @ViewChild('navbarCollapse') navbarCollapse!: ElementRef;
 
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    // Close navbar on navigation link click
+    const navLinks = this.navbarCollapse.nativeElement.querySelectorAll('.nav-link');
+    navLinks.forEach((link: HTMLElement) => {
+      this.renderer.listen(link, 'click', () => this.closeNavbar());
+    });
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleDocumentClick(event: Event) {
+    if (
+      this.navbarCollapse &&
+      this.navbarCollapse.nativeElement.classList.contains('show') &&
+      !this.navbarCollapse.nativeElement.contains(event.target)
+    ) {
+      this.closeNavbar();
+    }
+  }
+
+  closeNavbar() {
+    const collapseEl = this.navbarCollapse.nativeElement;
+    if (collapseEl.classList.contains('show')) {
+      const collapse = new (window as any).bootstrap.Collapse(collapseEl);
+      collapse.hide();
+    }
+  }
 }
