d6603e875c4885a154be807df5614704990a4bf7
whitelightningdev <danielmommsen@hotmail.com>
2025-05-30 08:18:35 +0200

    added a cookies consent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { FooterComponent } from "./components/footer/footer.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { slideInAnimation } from './route-animations';
 import { DownloadsComponent } from './downloads/downloads.component';
+import { CookieConsentComponent } from "./components/cookie-consent/cookie-consent.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, NavbarComponent, ],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent, CookieConsentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
