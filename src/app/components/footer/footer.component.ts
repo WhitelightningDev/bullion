5bc2fe4a3cd78e8c50ab6e4d540ba1b3b0773314
whitelightningdev <danielmommsen@hotmail.com>
2025-05-30 21:47:59 +0200

    created the cookie settings for the user to accept or decline the setttings

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CookieSettingsComponent } from "../cookie-settings/cookie-settings.component";
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CookieSettingsComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
