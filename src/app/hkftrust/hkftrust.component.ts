e70279bc4ae914a376b754d935e06584459f8582
whitelightningdev <danielmommsen@hotmail.com>
2025-07-25 00:55:03 +0200

    `Added Bootstrap types and styles to Hkftrust component, updated navbar links and added dropdowns, and implemented a confirmation dialog for external link redirection.`

--- src/app/hkftrust/hkftrust.component.ts
+++ src/app/hkftrust/hkftrust.component.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './hkftrust.component.css'
 })
 export class HkftrustComponent {
+confirmRedirect(): void {
+    const userConfirmed = confirm(
+      'You are about to leave this site and go to the Hong Kong Trust registration page. Do you want to continue?'
+    );
+
+    if (userConfirmed) {
+      window.open('https://hongkongtrust.vercel.app/', '_blank');
+    }
+  }
 
 }
